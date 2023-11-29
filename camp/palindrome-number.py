6cf3c3371436819ab4b9906f4e4f3448df049418
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2023-11-29 19:26:17 +0300

    Added solution for Palindrome Number

---
+++ camp/palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+
+        x = str(x)
+        y = x
+        x = x[::-1]
+        if x == y:
+            return True
+        else:
+            return False
+
+        
