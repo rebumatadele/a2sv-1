298ec3ac61177451c9811d564245fc5f6d5e694c
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2023-11-29 19:21:02 +0300

    Added solution for Longest Common Prefix

---
+++ camp/longest-common-prefix.py
@@ -0,0 +1,11 @@
+# Time:  O(n * k), k is the length of the common prefix
+# Space: O(1)
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        for i in xrange(len(strs[0])):
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
+
