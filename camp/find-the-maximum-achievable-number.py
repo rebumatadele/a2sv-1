d0981b05548279a7768a048dc19cd3e9704aa151
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2023-12-01 19:44:19 +0300

    Added solution for Find the Maximum Achievable Number

---
+++ camp/find-the-maximum-achievable-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        opps = 1
+        x = 0
+        x = num + 2*t
+        # while opps <= t:
+        #     x += (num+1)+1
+        #     opps +=1
+        return x
+        
+
