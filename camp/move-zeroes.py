2fba54fc2526641118d6e1ccf6d2953d760e9048
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-01-02 19:24:33 +0300

    Added solution for Move Zeroes

---
+++ camp/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        holder = 0
+        seeker = 0
+        while seeker < len(nums):
+            if nums[seeker] != 0:
+                nums[holder], nums[seeker] = nums[seeker], nums[holder]
+                holder +=1
+            seeker +=1
+
+        
