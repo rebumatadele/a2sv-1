8f1af21d53d05728351b2802b33aa4be0e8fc02d
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-02-26 11:07:12 +0300

    Add solution for Fibonacci Number

--- camp/leetcode/fibonacci-number.py
+++ camp/leetcode/fibonacci-number.py
@@ -1,8 +1,5 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 1:
-            return 1
-        elif n == 0:
-            return 0
-        f = self.fib(n-1)  + self.fib(n-2)     
-        return f
+        if n == 1 or n == 0:
+            return n
+        return self.fib(n-1)  + self.fib(n-2)     
