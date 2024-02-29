6518cb32389f07035bd09880e044346b6673061b
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-02-29 18:11:39 +0300

    Add solution for Maximum Score After Splitting a String

---
+++ camp/leetcode/maximum-score-after-splitting-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        one = s.count('1')
+        zero = 0
+        score = 0
+        for i in range(1, len(s)):
+            if s[i-1] == '0':
+                zero += 1
+            if s[i-1] == '1':
+                one -= 1
+            score = max(score, zero + one)
+        return score
+
+
