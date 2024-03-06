700c9e25a6dabb8ffb3eb2de4b397f6fdb7c3e24
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-03-06 19:57:33 +0300

    Add solution for H-Index II

--- camp/leetcode/h-index-ii.py
+++ camp/leetcode/h-index-ii.py
@@ -4,10 +4,8 @@ class Solution:
         right = len(citations) -1
         while left <= right:
             mid = (left + right)//2
-            if citations[mid] < ((len(citations)) - mid):
+            if citations[mid] < len(citations) - mid:
                 left = mid + 1
             else:
                 right = mid -1
-        if left == -1:
-            left = 0
         return len(citations) - left
