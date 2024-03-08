597e62cde9765633b611932e2e006efcae63684f
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-03-08 19:05:48 +0300

    Add solution for Intersection of Two Linked Lists

--- camp/leetcode/intersection-of-two-linked-lists.py
+++ camp/leetcode/intersection-of-two-linked-lists.py
@@ -6,12 +6,15 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        s = set()
-        while headA:
-            s.add(headA)
-            headA = headA.next
-        while headB:
-            if headB in s:
-                return headB
-            headB = headB.next
-        return None
+        a = headA
+        b = headB
+        while a != b:
+            if a:
+                a = a.next
+            else:
+                a = headB
+            if b:
+                b = b.next
+            else:
+                b = headA
+        return b
