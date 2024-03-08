dd030a5e67ed641b4ebbf8874e7e7b05ce73c12c
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-03-08 15:54:40 +0300

    Add solution for Maximum Binary Tree

---
+++ camp/leetcode/maximum-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        m = max(nums)
+        node = TreeNode(m)
+        indx = nums.index(m)
+        node.left = self.constructMaximumBinaryTree(nums[:indx])
+        node.right = self.constructMaximumBinaryTree(nums[indx + 1:])
+        return node
