51497919b433f48137ba11f6b2b2b53875c60e00
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-04-16 19:13:44 +0300

    Time: 81 ms (90.99%) | Memory: 20.7 MB (98.88%) - LeetSync

--- DSA_second_phase/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ DSA_second_phase/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,12 +3,6 @@ class Solution:
         dic = defaultdict(int)
         for i in nums:
             dic[i] += 1
-        print(dic)
         nums = list(set(nums))
         nums.sort(key = lambda x: dic[x], reverse = True)
-        print(nums)
-        ans = []
-        for i in nums:
-            if not ans or ans[-1] != i:
-                ans.append(i)
-        return ans[:k]
+        return nums[:k]
