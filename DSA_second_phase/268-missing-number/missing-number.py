f80446cb8ea492614f5a5a153680eda1b258be6c
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-04-30 19:11:32 +0300

    Time: 111 ms (61.51%) | Memory: 17.9 MB (30.98%) - LeetSync

--- DSA_second_phase/268-missing-number/missing-number.py
+++ DSA_second_phase/268-missing-number/missing-number.py
@@ -1,5 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        s = sum(nums)
-        n = (len(nums) * (len(nums) + 1)) /2
-        return int(n - s)
+        # s = sum(nums)
+        # n = (len(nums) * (len(nums) + 1)) /2
+        # return int(n - s)
+        nums.sort()
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return max(nums) + 1
+            
