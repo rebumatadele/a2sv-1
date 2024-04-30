b82aef615e48b4a9d5044e31381b9d8bd026e37a
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-04-30 18:30:46 +0300

    Time: 74 ms (33.58%) | Memory: 23.1 MB (84.95%) - LeetSync

---
+++ DSA_second_phase/338-counting-bits/counting-bits.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0 for _ in range(n+1)]
+        # for i in range(n+1):
+        #     last = i % 2
+        #     cnt = 0
+        #     origional = i
+        #     while i > 1:
+        #         j = i
+        #         i = i//2
+        #         if j != i * 2:
+        #             cnt += 1
+        #     if last:
+        #         cnt += 1
+        #     ans[origional] = cnt
+        # return ans
+        print(ans)
+        for i in range(n+1):
+
+            if i % 2 == 1:
+                ans[i] = ans[i -1] + 1
+            else:
+                ans[i] = ans[i // 2 ]
+                # print("dfg")
+        return ans
+
+
+                
+                
