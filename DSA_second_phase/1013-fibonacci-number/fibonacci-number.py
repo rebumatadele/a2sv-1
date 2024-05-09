dfb472bf93754ec4e29f6cbdcae2f6ea27c9b4b3
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-05-09 17:50:52 +0300

    Time: 40 ms (49.13%) | Memory: 16.5 MB (82.80%) - LeetSync

--- DSA_second_phase/1013-fibonacci-number/fibonacci-number.py
+++ DSA_second_phase/1013-fibonacci-number/fibonacci-number.py
@@ -1,12 +1,11 @@
 class Solution:
+    def __init__(self):
+        self.memo = defaultdict()
     def fib(self, n: int) -> int:
-        def helper(i):
-            if i == 0 or i == 1:
-                return i
-            elif i not in memo:
-                memo[i] = helper(i - 1) + helper(i-2)
-            return memo[i]
-        memo = defaultdict(int)
-        return helper(n)
+        if n == 0 or n == 1:
+            return n
+        elif n not in self.memo:
+            self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
+        return self.memo[n]
 
         
