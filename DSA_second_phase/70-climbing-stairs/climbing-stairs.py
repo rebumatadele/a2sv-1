5d7b19d393dc4bb880bb9d89fd1c471a7bc7ea83
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-05-09 18:28:21 +0300

    Time: 35 ms (51.18%) | Memory: 16.5 MB (30.50%) - LeetSync

--- DSA_second_phase/70-climbing-stairs/climbing-stairs.py
+++ DSA_second_phase/70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        @cache
         def helper(i):
             if i == 1 or i == 2:
                 return i
-            if i not in memo:
-                memo[i] = helper(i-1) + helper(i-2)
-            return memo[i]
-            
-        memo = defaultdict(int)
+            return helper(i-1) + helper(i-2)  
         return helper(n)
