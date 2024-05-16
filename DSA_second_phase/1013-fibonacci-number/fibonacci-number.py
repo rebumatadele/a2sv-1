e88e26f3badd08a666be7a7c5920643bdc82acd1
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-05-16 18:08:36 +0300

    Time: 41 ms (45.29%) | Memory: 16.4 MB (82.95%) - LeetSync

--- DSA_second_phase/1013-fibonacci-number/fibonacci-number.py
+++ DSA_second_phase/1013-fibonacci-number/fibonacci-number.py
@@ -2,7 +2,7 @@ class Solution:
     def fib(self, n: int) -> int:
         que = deque([0, 1])
         ans = 1
-        if n == 0 or n == 1:
+        if n<2:
             return n
         for i in range(2, n+1):
             ans = sum(que)
