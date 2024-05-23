0e2c6b736a745e8f1c660f582519af78bb9ce3aa
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-05-23 17:29:57 +0300

    Time: 45 ms (58.82%) | Memory: 16.6 MB (83.76%) - LeetSync

--- DSA_second_phase/909-stone-game/stone-game.py
+++ DSA_second_phase/909-stone-game/stone-game.py
@@ -1,7 +1,3 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        n = len(piles)
-        ans = 0
-        for i in range(1, n // 2 + 1):
-            ans += max(piles[i] - piles[n - i], piles[n - i] - piles[i])
-        return ans > 0
+        return True
