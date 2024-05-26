15389eec7c6893529b3597f3fb03060574ad48f1
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-05-26 15:47:10 +0300

    Time: 97 ms (91.99%) | Memory: 26.5 MB (27.61%) - LeetSync

---
+++ DSA_second_phase/403-frog-jump/frog-jump.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canCross(self, stones: list[int]) -> bool:
+        s = set(stones)
+
+        @cache
+        def jump(i, k):
+            if i == stones[-1]:
+                return True
+            for x in range(k - 1, k + 2):
+                if x and x + i in s and jump(i + x, x):
+                    return True
+            return False
+
+        
+        return jump(stones[0], 0)
