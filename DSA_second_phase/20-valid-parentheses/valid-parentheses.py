f9997bcd6b260bb80865d2a3dfc05cbeae04b36a
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-12-01 11:19:57 +0300

    Time: 0 ms (100.00%) | Memory: 17.2 MB (5.12%) - LeetSync

---
+++ DSA_second_phase/20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        charmap = {
+            "]" : "[",
+            "}" : "{",
+            ")" : "("
+        }
+        for el in s:
+            if el != ")" and el != "}" and el != "]":
+                stack.append(el)
+                continue
+            if not stack or stack[-1] != charmap[el]:
+                return False
+            stack.pop() 
+        if stack:
+            return False
+        return True
