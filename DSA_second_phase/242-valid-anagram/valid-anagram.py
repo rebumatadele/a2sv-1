33d0d855bc7f569c4877db42a566c2c35eba7fe6
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-12-01 11:30:21 +0300

    Time: 15 ms (48.85%) | Memory: 18.1 MB (5.96%) - LeetSync

--- DSA_second_phase/242-valid-anagram/valid-anagram.py
+++ DSA_second_phase/242-valid-anagram/valid-anagram.py
@@ -1,11 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # if len(s) != len(t):
-        #     return False
-        # char_count_s = Counter(s)
-        # char_count_t = Counter(t)
-        # for key, val in char_count_s.items():
-        #     if val != char_count_t[key]:
-        #         return False
-        # return True
         return sorted(s) == sorted(t)
