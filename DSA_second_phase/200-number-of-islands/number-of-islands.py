93bbec2d8be8ee21fe88a41006383ed29b03e392
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2024-12-05 07:34:52 +0300

    Time: 289 ms (17.86%) | Memory: 25.1 MB (16.70%) - LeetSync

--- DSA_second_phase/200-number-of-islands/number-of-islands.py
+++ DSA_second_phase/200-number-of-islands/number-of-islands.py
@@ -1,27 +1,34 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        1. Use a set to keep the vissisted positions
+        2. We will use a queue since we are using a BFS
+        """ 
+        islands = 0
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
+
         def inbound(r, c):
-            return ((0 <= r < len(grid)) and (0 <= c < len(grid[0])))
+            return (0<= r < rows) and (0 <= c < cols)
+        def bfs(r, c):
+            que = deque()
+            visited.add((r, c))
+            que.append((r,c))
 
-        def land(r,c):
-            return grid[r][c] == "1"
+            while que:
+                row, col = que.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if inbound(r, c) and grid[r][c] == "1" and (r, c) not in visited:
+                        que.append((r, c))
+                        visited.add((r, c))
 
-        def dfs(r, c):
-            visited[r][c] = True
 
-            for row_change, col_change in dxn:
-                new_row = r + row_change
-                new_col = c + col_change
 
-                if inbound(new_row, new_col) and land(new_row, new_col) and not visited[new_row][new_col]:
-                    dfs(new_row, new_col)
-
-        dxn = [(0,1), (0,-1), (1,0), (-1,0)]
-        visited = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
-        ans = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == "1" and not visited[r][c]:
-                    dfs(r, c)
-                    ans += 1
-        return ans
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    islands += 1
+                    bfs(r, c)
+        return islands
