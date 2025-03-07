2896a31057ffb03442f65835e21868a0f059a724
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2025-03-07 08:44:44 +0300

    Time: 7 ms (37.22%) | Memory: 19.6 MB (6.51%) - LeetSync

--- DSA_second_phase/1-two-sum/two-sum.py
+++ DSA_second_phase/1-two-sum/two-sum.py
@@ -1,9 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_store = defaultdict(int)
-        for indx, el in enumerate(nums):
-            difference = target - el
-            if difference in nums_store.keys():
-                return [nums_store[difference], indx]
-            nums_store[el] = indx
-        return []
+        num_lst = []
+        for idx, el in enumerate(nums):
+            num_lst.append((el, idx))
+        
+        num_lst.sort()
+        i = 0
+        j = len(num_lst) - 1
+        while i < j:
+            current = num_lst[i][0] + num_lst[j][0]
+            if current == target:
+                return [num_lst[i][1], num_lst[j][1]]
+            elif current > target:
+                j -= 1
+            else:
+                i += 1
+        
