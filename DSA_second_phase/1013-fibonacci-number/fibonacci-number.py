0622be128e40fd589b796b5f688fe1e10ce7f33b
Rebuma Tadele <118250996+rebumatadele@users.noreply.github.com>
2025-03-10 09:49:37 +0300

    Time: 418 ms (5.06%) | Memory: 17.9 MB (24.39%) - LeetSync

--- DSA_second_phase/1013-fibonacci-number/fibonacci-number.py
+++ DSA_second_phase/1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,6 @@
 class Solution:
-    def __init__(self):
-        self.memo = defaultdict(int)
     def fib(self, n: int) -> int:
         if n == 0 or n == 1:
             return n
-        elif n not in self.memo:
-            self.memo[n] = self.fib(n - 1) + self.fib(n - 2)
-        return self.memo[n]
-
-        
+        current = self.fib(n-1) + self.fib(n-2)
+        return current 
